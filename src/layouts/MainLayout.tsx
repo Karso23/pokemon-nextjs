80550ad706485facfe1022169a473f85a587d68a
Karso23 <karsoreef@hotmail.com>
2023-02-17 00:17:36 -0600

    added meta:og elements

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,6 +6,8 @@ interface Props {
   title?: string;
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin
+
 export const MainLayout = ({ children, title }: PropsWithChildren<Props>) => {
   return (
     <>
@@ -14,6 +16,9 @@ export const MainLayout = ({ children, title }: PropsWithChildren<Props>) => {
         <meta name='author' content='Karso23' />
         <meta name='description' content={`Informacion sobre el pokemon ${title}`} />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+        <meta property='og:title' content={`Info About ${title}`} />
+        <meta property='og:description' content={`This page is about ${title}`} />
+        <meta property='og:image' content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar />
       <main style={{ padding: '0px 20px' }}>
